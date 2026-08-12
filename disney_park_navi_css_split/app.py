3838d4d0e27c11776d8cf941f08c6751fcc78291
yuyajames133 <137299664+yuyajames133@users.noreply.github.com>
2026-08-12 18:09:36 +0900

    Update app.py

--- disney_park_navi_css_split/app.py
+++ disney_park_navi_css_split/app.py
@@ -2385,7 +2385,7 @@ def make_overview_map(frame, location, favorites):
         # 施設の緯度の平均
         map_lat = frame["lat"].mean()
         # 施設の軽度の平均
-        map_lat = frame["lon"].mean()
+        map_lot = frame["lon"].mean()
     else:
         #施設が一件もない場合は
         #現在地を地図の中心にする
